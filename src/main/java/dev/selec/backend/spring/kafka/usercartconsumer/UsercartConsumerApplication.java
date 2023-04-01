466e24e56ad74262000efe6dbb2c742e2c4dcd01
SelecGH <victormoares@gmail.com>
2023-04-01 20:27:37 +0200

    Feat: Initial commit

---
+++ src/main/java/dev/selec/backend/spring/kafka/usercartconsumer/UsercartConsumerApplication.java
@@ -0,0 +1,13 @@
+package dev.selec.backend.spring.kafka.usercartconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercartConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsercartConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/selec/backend/spring/kafka/usercartconsumer/UsercartConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.selec.backend.spring.kafka.usercartconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercartConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
